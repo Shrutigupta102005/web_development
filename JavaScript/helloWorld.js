ceabe434056e4cdbca1b2c6db4436d43900fc1eb
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-12-24 19:06:33 +0530

    .

--- JavaScript/helloWorld.js
+++ JavaScript/helloWorld.js
@@ -2,3 +2,30 @@
 var a = 19;
 let b = 20;
 const c=21;
+console.log(a);
+
+
+
+// functions
+// function creation
+function saymyname(){
+    console.log("shruti gupta");
+}
+// function call
+saymyname()
+
+// function with parameter
+
+function avg(num1,num2){
+    console.log(num1+num2);
+}
+avg(23,45);
+
+// return function
+function sum(num1,num2,num3){
+    return(num1+num2+num3);
+    // unreachable statment
+    let nun = "else";
+}
+ans = sum(2,3,4);
+console.log(ans);
