ca7e74866f1b2abadf26f5136ba193c36dac347f
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-13 10:05:50 +0530

    ..

--- JavaScript/dtype.js
+++ JavaScript/dtype.js
@@ -4,3 +4,29 @@
 
 // ecma - standards for js 
 
+/*
+number - 2 to power 53
+bigint 
+string="
+boolean=true/false
+null = Standalone value
+undefined 
+symbol --> will be used in react    react
+object " */
+console.log(typeof(1));
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
+
+let score = 33;
+console.log(typeof(score));
+
+// dtype conversion 
+let num = Number(34);
+console.log(typeof(score));
+
+/*"33" = "33
+"33abc = nan
+true = 1
+false = 0 
+" */
+// NAN - not a number
