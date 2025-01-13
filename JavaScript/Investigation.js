704b4077a14f119273091fb0eef259225c9f2a1d
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-13 09:36:46 +0530

    ..

--- JavaScript/Investigation.js
+++ JavaScript/Investigation.js
@@ -3,3 +3,9 @@ console.log("Secret mission")
 const account_id = 1234;
 let email = "shrutig1002205@gmail.com";
 var account_password="shrutig10";
+// in js jab hum var use karte the tuh scope ki problem atti hai tuh use hatane ke liye hum hum var use nhi karte sirf let aur const 
+accountcity = "Sahraranpur"; // possible but not good to use 
+let account_state ; // value is undefined 
+/* */
+console.log("Secret mission");
+console.table({account_id,email,account_password,accountcity});
